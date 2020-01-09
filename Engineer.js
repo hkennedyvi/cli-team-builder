9e1d96d77befc245df7ed329c6a7b92c4befb70b
Howard Kennedy <hkennedyvi@gmail.com>
2020-01-09 12:31:24 -0800

    Engineer test passes, Intern test created and runs

--- Engineer.js
+++ Engineer.js
@@ -1,7 +1,7 @@
 const Employee = require("./Employee")
 
 class Engineer extends Employee {
-    constructor (github, role) {
+    constructor (name, id, email, github, role) {
         super("Annie", 1, "annie@gmail.com");
         this.github = github;
         this.role = role;
