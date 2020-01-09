2209c2c884337fb63e5d16823739b4df228d0574
Howard Kennedy <hkennedyvi@gmail.com>
2020-01-09 13:17:16 -0800

    app.js file created

---
+++ app.js
@@ -0,0 +1,9 @@
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const inquirer = require("inquirer");
+
+const engineer = new Engineer("Joe", 1, "joe@gmail.com", "git");
+
+console.log(engineer);
