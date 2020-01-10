555c33014ca360097b5d92eca03ba8042e11e8c1
Howard Kennedy <hkennedyvi@gmail.com>
2020-01-09 23:31:35 -0800

    First inquirer prompt questions added

--- app.js
+++ app.js
@@ -7,3 +7,33 @@ const inquirer = require("inquirer");
 const engineer = new Engineer("Joe", 1, "joe@gmail.com", "git");
 
 console.log(engineer);
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "confirm",
+            name: "employed",
+            message: "Are you an employee?"
+        }
+    ]).then(function ({ employed }) {
+//This if statement isn't reading variable, condition not being met
+        if (employed = true) { 
+            inquirer.prompt([
+                {
+                    type: "list",
+                    name: "position",
+                    message: "What is your position?",
+                    choices: [
+                        "Engineer",
+                        "Intern",
+                        "Manager",
+                        "Ainslee"
+                    ]
+                }
+            ]);
+        }
+    });
+
+};
+
+promptUser();
