e3abf4e4930ea3497593784200f9f1df33d56900
Howard Kennedy <hkennedyvi@gmail.com>
2020-01-13 12:34:01 -0800

    Inquirer changed, begins with manager, adds team members after

--- app.js
+++ app.js
@@ -4,51 +4,166 @@ const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const inquirer = require("inquirer");
 
-const engineer = new Engineer("Joe", 1, "joe@gmail.com", "git");
-
-console.log(engineer);
-
-function promptUser() {
+function startTeam() {
     return inquirer.prompt([
         {
-            type: "confirm",
-            name: "employed",
-            message: "Are you an employee?"
+            type: "input",
+            name: "name",
+            message: "What is your manager's name?"
+        }, {
+            type: "input",
+            name: "id",
+            message: "What is their ID?"
+        }, {
+            type: "input",
+            name: "email",
+            message: "What is their email?"
+        }, {
+            type: "input",
+            name: "office",
+            message: "What is their office number?"
         }
-    ]).then(function (firstAnswer) {
-        console.log(firstAnswer.employed);
-        //This if statement isn't reading variable, condition not being met
-        if (firstAnswer.employed = true) {
-            return inquirer.prompt([
-                {
-                    type: "list",
-                    name: "position",
-                    message: "What is your position?",
-                    choices: [
-                        "Engineer",
-                        "Intern",
-                        "Manager"
-                    ]
-                }
-            ]);
+    ]).then(function (managerAns) {
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "position",
+                message: "What team member position would you like to add?",
+                choices: [
+                    "Intern",
+                    "Engineer",
+                    "I don't want to add anymore team members"
+                ]
+            }
+        ]).then(function (answer) {
+             switch(answer.position) {
+            case "Intern":
+              console.log("YOU ARE AN INTERN");
+              addIntern();// code block
+              break;
+            case "Engineer":
+              console.log("YOU ARE AN ENGINEER");
+              addEngineer();// code block
+              break;
+            default:
+              console.log("YOUR TEAM IS BUILT");
+              constructTeam();// code block
+          }
+        })
+        function addIntern() {
+            console.log("Congrats, Intern");
         }
-    }).then(function (secondAnswer) {
-        console.log(secondAnswer.position);
-        //This if statement isn't reading variable, condition not being met
-        if (secondAnswer.position = "Intern") {
-            return inquirer.prompt([
-                {
-                    type: "input",
-                    name: "school",
-                    message: "What school did you attend?"
-                }
-            ])
+        function addEngineer() {
+            console.log("Congrats, Engineer");
         }
-    });
+        function constructTeam() {
+            console.log("Congrats, Team");
+        }
 
+       
+    })
 };
 
-promptUser();
+startTeam();
+
+
+
+// const questions = [
+//     {
+//         type: "confirm",
+//         name: "employed",
+//         message: "Are you an employee?"
+//     },{
+//         type: "list",
+//         name: "position",
+//         message: "What is your position?",
+//         choices: [
+//             "Engineer",
+//             "Intern",
+//             "Manager"
+//         ]
+//     },{
+//         type: "input",
+//         name: "school",
+//         message: "What school did you attend?",
+//         when : function(answers) {
+//             if (answers.position === "Intern") {
+//                 return questions.school;
+//             };
+//           }
+//     }
+// ]
+
+// // const positionQ = [
+// //     {
+// //         type: "list",
+// //         name: "position",
+// //         message: "What is your position?",
+// //         choices: [
+// //             "Engineer",
+// //             "Intern",
+// //             "Manager"
+// //         ],
+// //         when : function(answers) {
+// //             if (answers.employed == true) {
+// //                 return positionQ;
+// //             }
+// //         }
+// //     }
+// // ]
+
+
+
+// function promptApplicant () {
+//     return inquirer.prompt(questions, function(answers) {
+//         console.log(answers);
+//     });
+// };
+
+// promptApplicant();
+
+
+// // function promptUser() {
+// //     return inquirer.prompt([
+// //         {
+// //             type: "confirm",
+// //             name: "employed",
+// //             message: "Are you an employee?"
+// //         }
+// //     ]).then(function (firstAnswer) {
+// //         console.log(firstAnswer.employed);
+// //         //This if statement isn't reading variable, condition not being met
+// //         if (firstAnswer.employed = true) {
+// //             return inquirer.prompt([
+// //                 {
+// //                     type: "list",
+// //                     name: "position",
+// //                     message: "What is your position?",
+// //                     choices: [
+// //                         "Engineer",
+// //                         "Intern",
+// //                         "Manager"
+// //                     ]
+// //                 }
+// //             ]);
+// //         }
+// //     }).then(function (secondAnswer) {
+// //         console.log(secondAnswer.position);
+// //         //This if statement isn't reading variable, condition not being met
+// //         if (secondAnswer.position = "Intern") {
+// //             return inquirer.prompt([
+// //                 {
+// //                     type: "input",
+// //                     name: "school",
+// //                     message: "What school did you attend?"
+// //                 }
+// //             ])
+// //         }
+// //     });
+
+// // };
+
+// promptUser();
 
 
 
